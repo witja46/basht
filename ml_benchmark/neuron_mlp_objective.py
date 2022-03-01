0fa949142ec6a7193ce300084b6de35eadd953a0
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2022-03-01 12:00:24 +0100

    adjusted example script, added neuron

---
+++ ml_benchmark/neuron_mlp_objective.py
@@ -0,0 +1,73 @@
+import torch
+import tqdm
+from sklearn.metrics import classification_report
+from ml_benchmark.mlp import MLP
+import torch_neuron
+
+
+class NeuronMLPObjective:
+
+    def __init__(self, epochs, train_loader, val_loader, test_loader) -> None:
+        super().__init__()
+        self.train_loader = train_loader
+        self.val_laoder = val_loader
+        self.test_loader = test_loader
+        self.hyperparameters = None
+        self.epochs = epochs
+        self.device = None
+
+    def set_hyperparameters(self, hyperparameters):
+        self.hyperparameters = hyperparameters
+
+    def set_device(self, device_str):
+        self.device = torch.device(device_str)
+
+    def train(self):
+        # model setup
+        self.model = MLP(**self.hyperparameters)
+        self.model = self.model.to(self.device)
+        # train
+        epoch_losses = []
+        for epoch in tqdm.tqdm(range(1, self.epochs+1)):
+            batch_losses = []
+            for x, y in self.train_loader:
+                x = x.to(self.device)
+                y = y.to(self.device)
+                loss = self.model.train_step(x, y)
+                batch_losses.append(loss)
+            epoch_losses.append(sum(batch_losses)/len(batch_losses))
+        return epoch_losses
+
+    def validate(self):
+        self.model.eval()
+        self.model = torch_neuron.trace(
+            self.model, example_inputs=[self.val_laoder], dynamic_batch_size=True)
+        val_targets = []
+        val_preds = []
+        for x, y in self.val_laoder:
+            x = x.to(self.device)
+            y = y.to(self.device)
+            predictions = self.model.test_step(x)
+            targets = y.flatten().detach()
+            val_targets += [targets.detach()]
+            val_preds += [predictions.detach()]
+        val_targets = torch.cat(val_targets).cpu().numpy()
+        val_preds = torch.cat(val_preds).cpu().numpy()
+        return classification_report(val_targets, val_preds, output_dict=True, zero_division=1)
+
+    def test(self):
+        self.model.eval()
+        self.model = torch_neuron.trace(
+            self.model, example_inputs=[self.test_laoder], dynamic_batch_size=True)
+        test_targets = []
+        test_predictions = []
+        for x, y in self.test_loader:
+            x = x.to(self.device)
+            y = y.to(self.device)
+            predictions = self.model.test_step(x)
+            targets = y.flatten().detach()
+            test_targets += [targets.detach()]
+            test_predictions += [predictions.detach()]
+        test_targets = torch.cat(test_targets).cpu().numpy()
+        test_predictions = torch.cat(test_predictions).cpu().numpy()
+        return classification_report(test_targets, test_predictions, output_dict=True, zero_division=1)
