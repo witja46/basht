c35657c20ccd016b2a5103d329c6fcb139009c0d
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2022-06-02 19:10:08 +0200

    introduced benchmark class

--- experiments/experiment_cli.py
+++ experiments/experiment_cli.py
@@ -18,18 +18,7 @@ def main(hyperparameter, resources, config, epochs):
         config = {
                 "val_split_ratio": 0.2, "train_batch_size": 512, "val_batch_size": 128, "test_batch_size": 128}
 
-    task = MnistTask()
-    train_loader, val_loader, test_loader = task.create_data_loader(config)
-    objective_cls = MLPObjective
     grid = YMLParser.parse(hyperparameter)
-    grid["input_size"] = [task.input_size]
-    grid["output_size"] = [task.output_size]
-    objective_args = dict(
-        epochs=epochs,
-        train_loader=train_loader,
-        val_loader=val_loader,
-        test_loader=test_loader
-        )
     optimization_method = optimization_method_cls(
         objective_cls=objective_cls, objective_args=objective_args, grid=grid, resources=resources)
     optimization_method_results = optimization_method.main()
--- ml_benchmark/bench_runner.py
+++ ml_benchmark/bench_runner.py
@@ -1,42 +1,23 @@
 import base64
 import json
 import datetime
+from abc import ABC, abstractmethod
 
 from ml_benchmark.workload.mnist.mnist_task import MnistTask
 from ml_benchmark.metrics import Metrics
 
 
-class BenchmarkRunner():
-    task_registry = {"mnist": MnistTask}
+class Benchmark(ABC):
 
-    def __init__(self, bench_name: str, config: dict, task_str: str, grid: dict, resources: dict) -> None:
-        """
-            benchName: uniqueName of the bechmark, used in logging
-            config: configuration object
-        """
+    objective = None
+    grid = None
+    resources = None
 
-        self.bench_name = bench_name
-        # generate a unique name from the config
-        base64_bytes = base64.b64encode(json.dumps(config).encode('ascii'))
-        self.config_name = str(base64_bytes, 'ascii')
+    @abstractmethod
+    def __init__(self, objective, grid: dict, resources: dict) -> None:
+        pass
 
-        self.rundate = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        # perpare logger
-        self.logger = Metrics([self.bench_name, self.config_name, self.rundate], ["name", "config", "date"])
-
-        # objective set up
-        task = self.task_registry.get(task_str)()
-        train_loader, val_loader, test_loader = task.create_data_loader(config)
-        self.objective = task.objective_cls(
-            epochs=config["epochs"],
-            train_loader=train_loader, val_loader=val_loader, test_loader=test_loader)
-
-        # hyperparameter adding
-        grid["input_size"] = task.input_size
-        grid["output_size"] = task.output_size
-        self.grid = grid
-        self.resources = resources
-
+    @abstractmethod
     def deploy(self) -> None:
         """
             With the completion of this step the desired architecture of the HPO Framework should be running
@@ -45,12 +26,14 @@ class BenchmarkRunner():
         """
         pass
 
+    @abstractmethod
     def setup(self, *args, **kwargs):
         """
         Every Operation that is needed before the actual optimization (trial) starts and that is not relevant
         for starting up workers or the necessary architecture.
         """
 
+    @abstractmethod
     def run(self, *args, **kwargs):
         """
             Executing the hyperparameter optimization on the deployed platfrom.
@@ -58,12 +41,14 @@ class BenchmarkRunner():
         """
         pass
 
+    @abstractmethod
     def collect_trial_results(self, *args, **kwargs):
         """
         This step collects all necessary results from all performed trials. Necessary results are results that
         are used in order to retrieve the best hyperparameter setting and to collect benchmark metrics.
         """
 
+    @abstractmethod
     def test(self, *args, **kwargs):
         """
         This step tests the model instantiated with the best hyperparameter setting on the test split of the
@@ -71,6 +56,7 @@ class BenchmarkRunner():
         """
         pass
 
+    @abstractmethod
     def collect_benchmark_metrics(self, *args, **kwargs):
         """
             Describes the collection of all gathered metrics, which are not used by the HPO framework
@@ -79,6 +65,7 @@ class BenchmarkRunner():
         """
         pass
 
+    @abstractmethod
     def undeploy(self, *args, **kwargs):
         """
             The clean-up procedure to undeploy all components of the HPO Framework that were deployed in the
@@ -86,17 +73,56 @@ class BenchmarkRunner():
         """
         pass
 
+
+class BenchmarkRunner():
+    task_registry = {"mnist": MnistTask}
+
+    def __init__(self, benchmark_cls: Benchmark, task_str: str, config: dict, grid: dict, resources: dict) -> None:
+        """
+            benchName: uniqueName of the bechmark, used in logging
+            config: configuration object
+        """
+
+        # setup benchmark
+        task = self.task_registry.get(task_str)()
+        epochs = config.pop("epochs")
+        train_loader, val_loader, test_loader = task.create_data_loader(**config)
+        objective = task.objective_cls(
+            epochs=epochs,
+            train_loader=train_loader, val_loader=val_loader, test_loader=test_loader)
+
+        grid["input_size"] = task.input_size
+        grid["output_size"] = task.output_size
+        grid = grid
+        resources = resources
+        self.benchmark = benchmark_cls(objective, grid, resources)
+
+        self.bench_name = f"{task.__class__.__name__}__{self.benchmark_cls.__name__}"
+        # generate a unique name from the config
+        base64_bytes = base64.b64encode(json.dumps(config).encode('ascii'))
+        self.config_name = str(base64_bytes, 'ascii')
+
+        self.rundate = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        # perpare logger
+        self.logger = Metrics([self.bench_name, self.config_name, self.rundate], ["name", "config", "date"])
+        # TODO: add maximum available resources??
+
     def main(self, fname: str):
         self.logger.run_start = datetime.now()
-        self.deploy()
+        self.benchmark.deploy()
         self.logger.setup_start = datetime.now()
-        self.run(self.logger)
+        self.benchmark.run(self.logger)
         self.logger.run_end = datetime.now()
-
+        self.logger.setup_start = datetime.now()
+        self.benchmark.collect_trial_results()
+        self.logger.run_end = datetime.now()
+        self.logger.setup_start = datetime.now()
+        self.benchmark.test()
+        self.logger.run_end = datetime.now()
         self.logger.resultcollection_start = datetime.now()
-        self.collectMetrics()
+        self.benchmark.collect_benchmark_metrics
         self.logger.resultcollection_end = datetime.now()
-        self.undeploy()
+        self.benchmark.undeploy()
         self.logger.undeploy_end = datetime.now()
 
         if fname is not None:
