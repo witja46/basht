d15dbea5789a4b4de6187963ce08d35490191525
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2022-07-12 13:39:14 +0200

    added documentation

--- ml_benchmark/workload/objective.py
+++ ml_benchmark/workload/objective.py
@@ -28,10 +28,25 @@ class Objective(ABC):
 class ObjectiveHelper:
 
     def __init__(self) -> None:
+        """
+        The ObjectiveHelper serves as a Helper Class to setup the objective for a trial.
+        The Objective can be performed on a different execution environment, than the benchmark itself.
+        Therefore it needs to be ensured, that necessary information for tracking metrics are handed over to
+        the objective.
+        """
         # need possibility to set dynamically - public IP if ssh connect
         self.metrics_storage_adress = self._get_db_connection_string()
 
     def _get_db_connection_string(self):
+        """
+        Gets the connection string of the database from an environment variale. If the environment variable is
+        not set, the default value is retrieved from the metrics storage, as per assumption the objective
+        is exectued on within the same environment. If not please set "METRICS_DB_ADRESS" as a environment
+        variable. Otherwise the tracker will not be able to connect to the metrics storage.
+
+        Returns:
+            _type_: _description_
+        """
         connection_string = os.environ.get("METRICS_DB_ADRESS")
         if not connection_string:
             connection_string = MetricsStorage.connection_string
