284d25439fdb127aaf8bf665a5447e4da1abf107
basti.werner <werner@tu-berlin.de>
2022-07-20 18:13:30 +0200

    fixed issue in image builder

--- ml_benchmark/utils/image_build_wrapper.py
+++ ml_benchmark/utils/image_build_wrapper.py
@@ -41,6 +41,36 @@ class MinikubeImageBuilder(ImageBuildWrapper):
         return call.stdout.decode("utf-8").strip("\n")
 
     def cleanup(self, tag):
-        call = subprocess.run(["minikube", "delete",tag], shell=True, check=True)
+        call = subprocess.run(["minikube","image", "rm",tag], shell=True, check=True)
+        if call.returncode != 0:
+            raise Exception("Failed to cleanup")
+
+class DockerImageBuilder(ImageBuildWrapper):
+    def __init__(self) -> None:
+        pass
+
+    def deploy_image(self, image, tag, build_context):
+        call = subprocess.run(
+            ["docker", "build", "-t", tag,  "-f", image, "."], cwd=build_context,
+            capture_output=True)  # doesnt seem to work
+
+        if call.returncode != 0:
+            print(call.stderr.decode("utf-8").strip("\n"))
+            raise Exception("Failed to build image")
+
+        call = subprocess.run(
+            ["docker", "push",tag] ,check=True)  # doesnt seem to work
+
+        if call.returncode != 0:
+            print(call.stderr.decode("utf-8").strip("\n"))
+            raise Exception("Failed to deploy image")
+
+        print("IMAGE IMAGE ", call.stdout, call.stderr)
+
+        return call.stdout.decode("utf-8").strip("\n")
+
+    def cleanup(self, tag):
+        call = subprocess.run(
+            ["docker", "image","rm",tag], check=True)  # doesnt seem to work
         if call.returncode != 0:
             raise Exception("Failed to cleanup")
